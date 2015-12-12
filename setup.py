8805218ad7285212502693c82650e8dafdd0918c
Tom Barclay <tom@tombarclay.com>
2015-12-12 13:40:41 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,40 +6,29 @@ import sys
 from numpy.distutils.core import setup, Extension
 
 
+
 if sys.argv[-1] == "publish":
     os.system("python setup.py sdist upload")
     sys.exit()
 
 
-# First, make sure that the f2py interfaces exist.
-interface_exists = os.path.exists("ktransit/tmodtom.pyf")
-if "interface" in sys.argv or not interface_exists:
-    # Generate the Fortran signature/interface.
-    cmd = "cd ktransit;"
-    cmd += "f2py tmodtom.f -m _tmodtom -h tmodtom.pyf"
-    cmd += " --overwrite-signature"
-    os.system(cmd)
-    if "interface" in sys.argv:
-        sys.exit(0)
-
 # Define the Fortran extension.
-tmodtom = Extension("ktransit._tmodtom", ["ktransit/tmodtom.pyf", "ktransit/tmodtom.f"])
+tmodtom = Extension("ktransit._tmodtom", ["ktransit/tmodtom.f"])
 
 setup(
     name="ktransit",
+    description='A simple exoplanet transit modeling tool in python.',
     url="https://github.com/mrtommyb/ktransit",
     version="0.2.2",
     author="Tom Barclay",
     author_email="tom@tombarclay.com",
-    description="",
-    long_description="",
     packages=["ktransit", ],
     ext_modules=[tmodtom, ],
     classifiers=[
         # "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Developers",
         "Intended Audience :: Science/Research",
-        "License :: OSI Approved :: MIT License",
+        "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
         "Operating System :: OS Independent",
         "Programming Language :: Python",
     ],
