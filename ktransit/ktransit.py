4ac32c586d8ecab62ea32104a2e1c6e5376e0d3d
Sam Grunblatt <skgrunblatt@gmail.com>
2017-02-24 15:58:13 -1000

    removed xrange and iteritems

--- ktransit/ktransit.py
+++ ktransit/ktransit.py
@@ -42,10 +42,10 @@ class LCModel(object):
         self.zpt = zpt
 
     def update_star(self,**kwargs):
-        [setattr(self,k,v) for k,v in kwargs.iteritems()]
+        [setattr(self,k,v) for k,v in kwargs.items()]
 
     def update_planet(self,pnum,**kwargs):
-        for k,v in kwargs.iteritems():
+        for k,v in kwargs.items():
             valarr = getattr(self,k)
             valarr[pnum] = v
             setattr(self,k,valarr)
